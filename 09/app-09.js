185ac8b0956bd49fd572f47ec43bd28ada62234d
igna-zanella <igna.zanella@gmail.com>
2023-09-17 15:22:25 -0300

    corrección de indentación

--- 09/app-09.js
+++ 09/app-09.js
@@ -2,18 +2,20 @@
 const prompt = require("prompt-sync")()
 
 function positivoNegativo() {
-    let numero = prompt("Ingrese un número positivo o negativo: ")
-    
-    if (isNaN(numero)) {
-        console.log("No ingresó un número. Por favor, vuelva a intentarlo.")
-    } else { if ( numero == 0) {
-        console.log("Cero es un número neutro.")
-    } else { if (numero > 0){
-        console.log(numero + " es un número positivo")
+  let numero = prompt("Ingrese un número positivo o negativo: ");
+
+  if (isNaN(numero)) {
+    console.log("No ingresó un número. Por favor, vuelva a intentarlo.");
+  } else {
+    if (numero == 0) {
+      console.log("Cero es un número neutro.");
     } else {
-        console.log(numero + " es un número negativo")
+      if (numero > 0) {
+        console.log(numero + " es un número positivo");
+      } else {
+        console.log(numero + " es un número negativo");
+      }
     }
-    }
-}
+  }
 }
 positivoNegativo()
