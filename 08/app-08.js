d46992f4434a4d05923c7f6f987b68882a03523d
igna-zanella <igna.zanella@gmail.com>
2023-09-17 22:59:54 -0300

    Se reubicó parseFloat en la línea del prompt

--- 08/app-08.js
+++ 08/app-08.js
@@ -2,14 +2,14 @@
 const prompt = require("prompt-sync")()
 
 function promedio() {
-    let primerValor = prompt("Ingrese un número para promediar: ")
-    let segundoValor = prompt("Ingrese otro número para promediar: ")
-    let tercerValor = prompt("Ingrese un último número para promediar: ")
+    let primerValor = parseFloat(prompt("Ingrese un número para promediar: "))
+    let segundoValor = parseFloat(prompt("Ingrese otro número para promediar: "))
+    let tercerValor = parseFloat(prompt("Ingrese un último número para promediar: "))
     
     if ( isNaN(primerValor) || isNaN(segundoValor) || isNaN(tercerValor)) {
         console.log("No ingresó un número. Por favor, vuelva a intentarlo.")
     } else {
-    let resultado = ((parseFloat(primerValor) + parseFloat(segundoValor) + parseFloat(tercerValor)) / 3)
+    let resultado = ((primerValor + segundoValor + tercerValor) / 3)
     console.log("El promedio es: " + resultado)
     }
 }
