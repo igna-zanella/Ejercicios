da26b996120ed53fb43a4335b006f98de714489f
igna-zanella <igna.zanella@gmail.com>
2023-10-05 22:21:54 -0300

    Se convierte array de string a number con método map

--- 15/app-15.js
+++ 15/app-15.js
@@ -4,7 +4,8 @@
 const prompt = require("prompt-sync")()
 
 function maximo([n1, n2, n3]) {
-  let lista = [n1, n2, n3]
+  lista = [n1, n2, n3]
+  console.log(typeof lista[0])
   return Math.max(...lista)
 }
 
@@ -14,10 +15,11 @@ console.log(maximo([4, 5, 8]))
 console.log("Función entrando números por teclado para encontrar el valor máximo")
 function maximoPrompt() {
   let numeros = prompt("Ingrese números separados por un espacio: ")
-  let listaPrompt = numeros.split(" ")
+  let listaPrompt = numeros.split(" ").map(Number)
 
   // console.log(Math.max(...listaPrompt))
   console.log("Su lista ingresada fue: " + listaPrompt + " y el número máximo es " + Math.max(...listaPrompt))
+  console.log(typeof listaPrompt[0])
 }
 
 maximoPrompt()
